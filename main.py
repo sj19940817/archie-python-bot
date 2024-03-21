12889e695c446275df0f47364ec0a7e7874c30b4
sj19940817 <sj19940817@gmail.com>
2024-03-20 21:37:26 -0300

    modify comments

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ from telegram.ext import (
 
 # from .config import (Token)
 
-# Enable logging
+"""Enable logging"""
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
+
+"""set higher logging level for httpx to avoid all GET and POST requests being logged"""
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-#TG_Token = Token
 logger = logging.getLogger(__name__)
 
 CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
@@ -35,7 +35,6 @@ reply_keyboard = [
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
-
 def facts_to_str(user_data: Dict[str, str]) -> str:
     """Helper function for formatting the gathered user info."""
     facts = [f"{key} - {value}" for key, value in user_data.items()]
@@ -44,7 +43,7 @@ def facts_to_str(user_data: Dict[str, str]) -> str:
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Start the conversation and ask user for the data."""
     await update.message.reply_text(
-        "Hello! I am ArchieBot. Please input the token data for buying or selling",
+        "Hello! I am ArchieBot. Please input the data for buying or selling of tokens.",
         reply_markup=markup,
     )
 
@@ -57,7 +56,8 @@ async def select_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 
     reply_chains = [
         ["BSC"],
-        ["Avalanche"],
+        ["Avax"],
+        ["Solana"],
     ]
     chain_type = ReplyKeyboardMarkup(reply_chains, one_time_keyboard=True)
 
@@ -103,32 +103,23 @@ async def custom_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     return TYPING_CHOICE
 
 def is_valid_token_address(token_address: str)-> bool:
-#function to validate the TokenOutAddress
+    """function to validate the TokenOutAddress"""
     if len(token_address) != 42 or not token_address.startswith("0x"):
         return False
     
-    #Check if the address consists of valid hexadecimal characters
+    """Check if the address consists of valid hexadecimal characters"""
     try:
         int(token_address, 16)
         return True
     except ValueError:
         return False
 
-#Function to validate the private key
-def is_valid_private_key(private_key: str) -> bool:
-    private_key = private_key.strip()
+def is_valid_private_key(private_key):
+    """Function to validate the private key"""
+    return bool(re.match(r"^[A-Fa-f0-9]{64}$", private_key))
 
-    if not re.match('^[0-9a-fA-F]+$', private_key):
-        print('here=-----------')
-        return False
-
-    return True
-
-# def is_valid_private_key(private_key):
-#     return bool(re.match(r"^[A-Fa-f0-9]{64}$", private_key))
-
-# Cancel the conversation and clear user data.
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancel the conversation and clear user data."""
     user_data = context.user_data
     user_data.clear()
     await update.message.reply_text(
@@ -137,8 +128,8 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     )
     return ConversationHandler.END
 
-# Update the received_information function to include private key validation
 async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Update the received_information function to include private key validation"""
     user_data = context.user_data
     text = update.message.text
     category = user_data["choice"]
@@ -158,7 +149,7 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
             await update.message.reply_text("Please enter a valid private key.")
             return TYPING_REPLY
 
-        # Mask the private key before displaying it
+        """Mask the private key before displaying it"""
         masked_private_key = text[:3] + "....."
         user_data[category] = masked_private_key
     else:
@@ -184,8 +175,8 @@ async def confirm(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
 async def OK(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int: 
      user_data = context.user_data
 
-     # check if all required inputs have been provided
      if "Chain" in user_data and "TokenOutAddress" in user_data and "BNB" in user_data and "Private Key" in user_data:
+         """check if all required inputs have been provided"""
          reply_confirmation = [["Confirm", "Cancel"]]
          confirmation_markup = ReplyKeyboardMarkup(reply_confirmation, one_time_keyboard=True)
 
@@ -198,6 +189,10 @@ async def OK(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
          await update.message.reply_text(f"Please provide all required input data before confirming.. \n Your input data: {facts_to_str(user_data)} ", reply_markup = markup)
          return CHOOSING
 
+# # Function to send a new message with the updated ReplyKeyboardMarkup after a button is clicked
+# async def update_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     new_markup = 
+
 async def quit_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_data = context.user_data
     user_data.clear()
@@ -209,10 +204,11 @@ async def quit_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 def main() -> None:
     """Run the bot."""
-    # Create the Application and pass it your bot's token.
+
+    """Create the Application and pass it your bot's token."""
     application = Application.builder().token("7071537775:AAFD479QCXCFL6DB8A3cQkFALRznXsAUG44").build()
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    """Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY"""
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
@@ -221,7 +217,8 @@ def main() -> None:
                     filters.Regex("^Chain$"), select_choice
                 ),
                 MessageHandler(filters.Regex("^BSC"), select_chain),
-                MessageHandler(filters.Regex("^Avalanche"), select_chain),
+                MessageHandler(filters.Regex("^Avax"), select_chain),
+                MessageHandler(filters.Regex("^Solana"), select_chain),
                 MessageHandler(
                     filters.Regex("^(TokenOutAddress|BNB|Private Key)$"), regular_choice
                 ),
@@ -250,7 +247,7 @@ def main() -> None:
 
     application.add_handler(conv_handler)
 
-    # Run the bot until the user presses Ctrl-C
+    """Run the bot until the user presses Ctrl-C"""
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
