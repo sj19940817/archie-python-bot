f7d0acae065c7b74593bb097137abc5802e0a903
sj19940817 <sj19940817@gmail.com>
2024-03-29 18:42:59 -0300

    make buy function for test

--- abi.py
+++ abi.py
@@ -1,35 +1,125 @@
-from bs4 import BeautifulSoup as bsp
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from bs4 import BeautifulSoup as bsp
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
 
-def tokenAbi(address, driver=None):
-    try:
-        filename = f"ABI_{address}.txt"
-        with open(f"data/{filename}") as f:
-                abi = f.readlines()
-                return abi[0]
-    except IOError:
-        abi = findAbi(address, driver)
-        return abi
+# def tokenAbi(address, driver=None):
+#     try:
+#         filename = f"ABI_{address}.txt"
+#         with open(f"data/{filename}") as f:
+#                 abi = f.readlines()
+#                 return abi[0]
+#     except IOError:
+#         abi = findAbi(address, driver)
+#         return abi
     
-def findAbi(address, driver):
-    url = f"https://bscscan.com/address/{address}#code"
-    if not driver:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+# def findAbi(address, driver):
+#     url = f"https://bscscan.com/address/{address}#code"
+#     if not driver:
+#         driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    driver.get(url)
+#     driver.get(url)
 
-    page_soup = bsp(driver.page_source, "html.parser")
+#     page_soup = bsp(driver.page_source, "html.parser")
 
-    abi_element = page_soup.find_all("pre", class_= "js-copytextarea2")
-    # abi_element = page_soup.find("pre", id = "js-copytextarea2")
-    abi = abi_element if abi_element else None
+#     abi_element = page_soup.find_all("pre", class_= "js-copytextarea2")
+#     # abi_element = page_soup.find("pre", id = "js-copytextarea2")
+#     abi = abi_element if abi_element else None
 
-    with open(f"data/ABI_{address}.txt", "w") as f:
-         f.write(abi[0].text)
+#     with open(f"data/ABI_{address}.txt", "w") as f:
+#          f.write(abi[0].text)
         
-    driver.delete_all_cookies()
-    driver.get("chrome://settings/clearBrowserData")
-    driver.quit()
+#     driver.delete_all_cookies()
+#     driver.get("chrome://settings/clearBrowserData")
+#     driver.quit()
 
-    return abi[0].text
+#     return abi[0].text
+
+
+ERC20_ABI = [
+    {
+        'constant': False,
+        'inputs': [
+            {'name': '_spender', 'type': 'address'},
+            {'name': '_value', 'type': 'uint256'}
+        ],
+        'name': 'approve',
+        'outputs': [{'name': 'success', 'type': 'bool'}],
+        'payable': False,
+        'stateMutability': 'nonpayable',
+        'type': 'function'
+    },
+    {
+        'constant': True,
+        'inputs': [],
+        'name': 'totalSupply',
+        'outputs': [{'name': 'supply', 'type': 'uint256'}],
+        'payable': False,
+        'stateMutability': 'view',
+        'type': 'function'
+    },
+    {
+        'constant': False,
+        'inputs': [
+            {'name': '_from', 'type': 'address'},
+            {'name': '_to', 'type': 'address'},
+            {'name': '_value', 'type': 'uint256'}
+        ],
+        'name': 'transferFrom',
+        'outputs': [{'name': 'success', 'type': 'bool'}],
+        'payable': False,
+        'stateMutability': 'nonpayable',
+        'type': 'function'
+    },
+    {
+        'constant': True,
+        'inputs': [],
+        'name': 'decimals',
+        'outputs': [{'name': 'digits', 'type': 'uint256'}],
+        'payable': False,
+        'stateMutability': 'view',
+        'type': 'function'
+    },
+    {
+        'constant': True,
+        'inputs': [{'name': '_owner', 'type': 'address'}],
+        'name': 'balanceOf',
+        'outputs': [{'name': 'balance', 'type': 'uint256'}],
+        'payable': False,
+        'stateMutability': 'view',
+        'type': 'function'
+    },
+    {
+        'constant': False,
+        'inputs': [
+            {'name': '_to', 'type': 'address'},
+            {'name': '_value', 'type': 'uint256'}
+        ],
+        'name': 'transfer',
+        'outputs': [{'name': 'success', 'type': 'bool'}],
+        'payable': False,
+        'stateMutability': 'nonpayable',
+        'type': 'function'
+    },
+    {
+        'constant': True,
+        'inputs': [
+            {'name': '_owner', 'type': 'address'},
+            {'name': '_spender', 'type': 'address'}
+        ],
+        'name': 'allowance',
+        'outputs': [{'name': 'remaining', 'type': 'uint256'}],
+        'payable': False,
+        'stateMutability': 'view',
+        'type': 'function'
+    },
+    {
+        'anonymous': False,
+        'inputs': [
+            {'indexed': True, 'name': '_owner', 'type': 'address'},
+            {'indexed': True, 'name': '_spender', 'type': 'address'},
+            {'indexed': False, 'name': '_value', 'type': 'uint256'}
+        ],
+        'name': 'Approval',
+        'type': 'event'
+    }
+]
--- buy.py
+++ buy.py
@@ -1,8 +1,7 @@
-import config
+import config as config
 import time
 
 def buyTokens(params):
-
 
    symbol = params.get("symbol")
    web3 = params.get("web3")
@@ -14,27 +13,44 @@ def buyTokens(params):
    private_key = params.get("private_key")
    BNB_amount = web3.to_wei(BNB_amount, 'ether')
 
-   print("here is buytoken==================>", params)
-   pancakeSwap_txn = contract_pancake.functions.swapExactETHForTokens(
-      0,
-      [WBNB_Address, token_to_buy_address],
-      wallet_address,
-      (int(time.time() + 10000))).build_transaction({
-         "from": wallet_address,
-         "value": BNB_amount,
-         "gas": 160000,
-         "gasPrice": web3.to_wei('5', 'gwei'),
-         "nonce": web3.eth.get_transaction_count(wallet_address)
-      })
+   # print("here is buytoken==================>", params)
+   # pancakeSwap_txn = contract_pancake.functions.swapExactETHForTokens(
+   #    0,
+   #    [WBNB_Address, token_to_buy_address],
+   #    wallet_address,
+   #    (int(time.time() + 10000))).build_transaction({
+   #       "from": wallet_address,
+   #       "value": BNB_amount,
+   #       "gas": 160000,
+   #       "gasPrice": web3.to_wei('5', 'gwei'),
+   #       "nonce": web3.eth.get_transaction_count(wallet_address)
+   #    })
    
-   signed_txn = web3.eth.account.sign_transaction(pancakeSwap_txn, private_key)
-   try:
-      tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-      result = [web3.toHex(tx_token), f"Bought {web3.fromWei(BNB_amount, 'ether')} BNB of {symbol}"]
-      return result
-   except ValueError as e:
-      if e.args[0].get("message") in "intrinsic gas too low":
-         result = ["Failed", f"ERROR: {e.args[0].get('message')}"]
-      else:
-         result = ["Failed", f"ERROR: {e.args[0].get('message')} : {e.args[0].get('code')}"]
-      return result
+   # signed_txn = web3.eth.account.sign_transaction(pancakeSwap_txn, private_key)
+   # try:
+   #    tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+   #    result = [web3.toHex(tx_token), f"Bought {web3.fromWei(BNB_amount, 'ether')} BNB of {symbol}"]
+   #    return result
+   # except ValueError as e:
+   #    if e.args[0].get("message") in "intrinsic gas too low":
+   #       result = ["Failed", f"ERROR: {e.args[0].get('message')}"]
+   #    else:
+   #       result = ["Failed", f"ERROR: {e.args[0].get('message')} : {e.args[0].get('code')}"]
+   #    return result
+
+
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from wev3.exceptions import TransactionNotFound
+
+#Initialize Web3 with BSC RPC endpoint
+bsc_rpc_ednpoint = "https://bsc-dataseed.binance.org/"
+web3 = Web3(Web3.HTTPProvider(bsc_rpc_ednpoint))
+web3.middleware_onion.inject(geth_poa_middleware, layer = 0)
+
+#Define contract addresses and ABIs for PancakeSwap Router and ERC20 token
+pancake_router_address = config.PANCAKE_ROUTER_ADDRESS
+pancake_router_abi = []
+erc20_abi = []
+
+#Define wallet address and private key
