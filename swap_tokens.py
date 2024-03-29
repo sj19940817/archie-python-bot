8c7a7ea5cd2780dc54e11e177709279d45414b98
sj19940817 <sj19940817@gmail.com>
2024-03-28 21:15:21 -0300

    3-38

--- swap_tokens.py
+++ swap_tokens.py
@@ -1,5 +1,4 @@
 # import time
-# import winsound
 from os import system
 
 from bs4 import BeautifulSoup as bsp
@@ -10,7 +9,7 @@ from web3 import Web3
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 
-# from buy import buyTokens
+from buy import buyTokens
 # from sell import sellTokens
 import config as config
 # from ThreadingWithReturn import ThreadWithResult
@@ -30,8 +29,8 @@ web3 = Web3(Web3.HTTPProvider(bsc))
 
 # Important Addresses
 # TokenToSellAddress = web3.toChecksumAddress(address)
-# WBNB_Address = web3.toChecksumAddress(config.WBNB_ADDRESS)
-pancakeRouterAddress = web3.to_checksum_address(config.PANCAKE_ROUTER_ADDRESS)
+# WBNB_Address = web3.to_checksum_address(config.WBNB_ADDRESS)
+pancake_router_address = web3.to_checksum_address(config.PANCAKE_ROUTER_ADDRESS)
 # walletAddress = config.walletAddress
 TradingTokenDecimal = None
 
@@ -44,48 +43,58 @@ TradingTokenDecimal = None
 # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options = options)
 driver = webdriver.Chrome()
 
-
-
 # def showTx(url):
 #     webdriver.Chrome(executable_apth = ChromeDriverManager().install()).get(url)
 
 def initializeTrade(user_data):
 
     global driver
-    # global TokenToSellAddress
     global TradingTokenDecimal
     print("user_data in swap_tokens",user_data)
     chain = user_data.get("Chain")
-    bnb = user_data.get("BNB")
+    WBNB_Address = user_data.get("BNB")
     token_to_buy_address = user_data.get("TokenToBuyAddress")
+    # WBNB_Address = web3.toChecksumAddress(WBNB_Address)
+    # token_to_buy_address = web3.toChecksumAddress(token_to_buy_address)
     wallet_address = user_data.get("Wallet Address")
     private_key = user_data.get("Private Key")
     # if "Add Comments" in user_data:
     #     comments = user_data.get("Add Comments")
     # print("comments on swap function",comments)    
 
-
     # Getting ABI
     buy_token_abi = tokenAbi(token_to_buy_address, driver)
-    pancakeAbi = tokenAbi(pancakeRouterAddress, driver)
-    print("selltoken_abi ----------------->",buy_token_abi)
+    pancake_abi = tokenAbi(pancake_router_address, driver)
+
     # Enter your wallet Public Address
     BNB_balance = web3.eth.get_balance(wallet_address)
     BNB_balance = web3.from_wei(BNB_balance, "ether")
 
-    # # Create a contract for both PancakeRoute and Token to Sell
-    contractPancake = web3.eth.contract(address = pancakeRouterAddress, abi = pancakeAbi)
-    contractSellToken = web3.eth.contract(token_to_buy_address, abi = buy_token_abi)
+    # Create a contract for both PancakeRoute and Token to Sell
+    contract_pancake = web3.eth.contract(address = pancake_router_address, abi = pancake_abi)
+    contract_buy_token = web3.eth.contract(token_to_buy_address, abi = buy_token_abi)
 
-    # if TradingTokenDecimal is None:
-    #     TradingTokenDecimal = contractSellToken.functions.decimals().call()
-    #     TradingTokenDecimal = getTokenDecimal(TradingTokenDecimal)
+    if TradingTokenDecimal is None:
+        TradingTokenDecimal = contract_buy_token.functions.decimals().call()
+        TradingTokenDecimal = getTokenDecimal(TradingTokenDecimal)
 
-    # # Get current available amount of tokens from the wallet
-    # NoOftokens = contractSellToken.functions.balanceOf(walletAddress).call()
-    # NoOftokens = web3.fromWei(NoOftokens, TradingTokenDecimal)
-    # symbol = contractSellToken.functions.symbol().call()
-    # # params = {
-    # #     "symbol": symbol,
-    # #     ""
-    # # }
+    # Get current avaliable amount of tokens from the wallet
+    NoOfTokens = contract_buy_token.functions.balanceOf(wallet_address).call()
+    NoOfTokens = web3.from_wei(NoOfTokens, TradingTokenDecimal)
+    print("NoOfTokens ====================>", NoOfTokens)
+    symbol = contract_buy_token.functions.symbol().call()
+    
+    params = {
+        "symbol": symbol,
+        "web3": web3,
+        "wallet_address": wallet_address,
+        "contract_buy_token": contract_buy_token,
+        "contract_pancake": contract_pancake,
+        "pancake_router_address": pancake_router_address,
+        "token_to_buy_address": token_to_buy_address,
+        "WBNB_Address": WBNB_Address,
+        "trading_token_decimal": TradingTokenDecimal 
+    } 
+    
+    buyTokens(params)
+    # return BNB_balance, symbol, NoOfTokens, params
